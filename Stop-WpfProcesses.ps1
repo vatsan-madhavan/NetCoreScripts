f2ac0039776ac05f95c45871006a154002894218
Vatsan Madhavan <vatsan.madhavan@microsoft.com>
2020-01-31 13:09:01 -0800

    Complete scrit to stop all .NET framework WPF processes

--- Stop-WpfProcesses.ps1
+++ Stop-WpfProcesses.ps1
@@ -34,11 +34,65 @@ function Use-RunAs {
     }  
 } 
 
+Function Kill-ChildProcesses {
+    [CmdletBinding(PositionalBinding=$false)]
+    param(
+        [int]$ProcessId,
+        [string]$Tabs="",
+        [switch]$DryRun
+    )
+
+    $processName = (Get-Process -Id $ProcessId).Name
+    Write-Host "$Tabs[$processName] $ProcessId"
+
+    Get-CimInstance Win32_Process | ? {
+        $_.ParentProcessId -eq $ProcessId
+    } | % {
+        Kill-ChildProcesses -ProcessId $_.ProcessId -Tabs ($Tabs + "`t") -DryRun:$DryRun
+    }
+
+    if (-not $DryRun) {
+        Stop-Process -Force -Id $processId
+    }
+}
+
+Function Kill-ChildProcessesByName {
+    [CmdletBinding(PositionalBinding=$false)]
+    param(
+        [string]$ProcessName,
+        [switch]$DryRun
+    )
+
+    Get-Process -Name $ProcessName -ErrorAction SilentlyContinue | % {
+        Kill-ChildProcesses -ProcessId $_.Id -DryRun:$DryRun
+    }
+}
+
 Use-RunAs
 
-Invoke-Expression "reg.exe ADD HKCU\Software\Sysinternals /v EulaAccepted /t REG_DWORD /d 1 /f"
-Invoke-Expression "reg.exe ADD HKU\.DEFAULT\Software\Sysinternals /v EulaAccepted /t REG_DWORD /d 1 /f"
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 
-$wpfDlls = & listdlls -d 'wpfgfx_v0400.dll' 
+Invoke-Expression "reg.exe ADD HKCU\Software\Sysinternals /v EulaAccepted /t REG_DWORD /d 1 /f" | Out-Null
+Invoke-Expression "reg.exe ADD HKU\.DEFAULT\Software\Sysinternals /v EulaAccepted /t REG_DWORD /d 1 /f" | Out-Null
 
-$wpfDlls
+if (-not (test-path -PathType Leaf $listDlls)) {
+    Kill-ChildProcessesByName -ProcessName ListDlls
+    Remove-Item -Path $listDlls -Force
+}
+
+Invoke-WebRequest "https://live.sysinternals.com/listdlls.exe" -OutFile $listDlls 
+
+$wpfDlls = & $listDlls -d wpfgfx_v0400.dll
+
+$processIDs = New-Object System.Collections.Generic.HashSet[int]
+$wpfDlls | % {
+    #[System.Text.RegularExpressions.MatchCollection]$matches = [regex]::Matches($_, "[\w\.]+\s+pid\:\s+(\d+)")
+    if ($_ -match "[\w\.]+\s+pid\:\s+(\d+)") {
+        [void]$processIDs.Add($Matches[1])
+    }
+}
+
+$processIDs | % {
+     Kill-ChildProcesses -ProcessId $_
+}
