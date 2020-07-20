3151e20d922384df6b9f94d3546d32166d620419
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2020-07-20 16:43:38 -0700

    Update ps1 scripts

--- VSDevCmd/VSDevCmd.ps1
+++ VSDevCmd/VSDevCmd.ps1
@@ -6,23 +6,70 @@
 
     To see tool help, call with '/?' (not '-?'). Also see examples.
 .PARAMETER Command
-   Command to run
+    Application/Command to execute in the VS Developer Command Prompt Environment
+.PARAMETER Arguments
+    Arguments to pass to Application/Command being executed
+.PARAMETER VisualStudioEdition
+    Selects Visual Studio Development Environment based on Edition (Community, Professional, Enterprise, etc.)
+.PARAMETER VisualStudioVersion
+    Selects Visual Studio Development Environment based on Version (2015, 2017, 2019 etc.)
+.PARAMETER VisualStudioCodename
+    Selects Visual Studio Development Environment based on Version CodeName (Dev14, Dev15, Dev16 etc.)
+.PARAMETER VisualStudioBuildVersion
+    Selects Visual Studio Development Environment based on Build Version (e.g., "15.9.25", "16.8.0").
+    A prefix is sufficient (e.g., "15", "15.9", "16" etc.)
+.PARAMETER Interactive
+    Runs in interactive mode. Useful for running programs like cmd.exe, pwsh.exe, powershell.exe or csi.exe in the Visual Studio Developer Command Prompt Environment
 .EXAMPLE
     PS C:\> .\VSBuild.ps1 msbuild /?
     
     Runs 'msbuild /?' 
 #>
+[CmdletBinding(DefaultParameterSetName='Default')]
 param (
-    [Parameter(Mandatory=$true, Position = 0)]
+    [Parameter(ParameterSetName = 'Default', Position=0 ,Mandatory=$true, HelpMessage='Application or Command to Run')]
+    [Parameter(ParameterSetName = 'CodeName', Position=0 ,Mandatory=$true, HelpMessage='Application or Command to Run')]
     [string]
     $Command, 
 
-    [Parameter(Position=1, ValueFromRemainingArguments)]
+    [Parameter(ParameterSetName = 'Default', Position=1, ValueFromRemainingArguments, HelpMessage='List of arguments')]
+    [Parameter(ParameterSetName = 'CodeName', Position=1, ValueFromRemainingArguments, HelpMessage='List of arguments')]
     [string[]]
-    $Arguments, 
+    $Arguments,
 
-    [switch]
+    [Parameter(ParameterSetName='Default', Mandatory = $false, HelpMessage='Selects Visual Studio Development Environment based on Edition (Community, Professional, Enterprise, etc.)')]
+    [Parameter(ParameterSetName='CodeName', Mandatory = $false, HelpMessage='Selects Visual Studio Development Environment based on Edition (Community, Professional, Enterprise, etc.)')]
     [CmdletBinding(PositionalBinding=$false)]
+    [Alias('Edition')]
+    [ValidateSet('Community', 'Professional', 'Enteprise', $null)]
+    [string]
+    $VisualStudioEdition = $null,
+
+    [Parameter(ParameterSetName='Default', Mandatory = $false, HelpMessage='Selects Visual Studio Development Environment based on Version (2015, 2017, 2019 etc.)')]
+    [CmdletBinding(PositionalBinding=$false)]
+    [Alias('Version')]
+    [ValidateSet('2015', '2017', '2019', $null)]
+    [string]
+    $VisualStudioVersion = $null, 
+
+    [Parameter(ParameterSetName='CodeName', Mandatory = $false, HelpMessage='Selects Visual Studio Development Environment based on Version CodeName (Dev14, Dev15, Dev16 etc.)')]
+    [CmdletBinding(PositionalBinding=$false)]
+    [Alias('CodeName')]
+    [ValidateSet('Dev14', 'Dev15', 'Dev16', $null)]
+    [string]
+    $VisualStudioCodeName=$null, 
+
+    [Parameter(ParameterSetName='Default', Mandatory = $false, HelpMessage='Selects Visual Studio Development Environment based on Build Version (e.g., "15.9.25", "16.8.0"). A prefix is sufficient (e.g., "15", "15.9", "16" etc.)')]
+    [Parameter(ParameterSetName='CodeName', Mandatory = $false, HelpMessage='Selects Visual Studio Development Environment based on Build Version (e.g., "15.9.25", "16.8.0"). A prefix is sufficient (e.g., "15", "15.9", "16" etc.)')]
+    [Alias('BuildVersion')]
+    [CmdletBinding(PositionalBinding=$false)]
+    [string]
+    $VisualStudioBuildVersion = $null, 
+
+    [Parameter(ParameterSetName='Default', HelpMessage='Runs in interactive mode. Useful for running programs like cmd.exe, pwsh.exe, powershell.exe or csi.exe in the Visual Studio Developer Command Prompt Environment')]
+    [Parameter(ParameterSetName='CodeName', HelpMessage='Runs in interactive mode. Useful for running programs like cmd.exe, pwsh.exe, powershell.exe or csi.exe in the Visual Studio Developer Command Prompt Environment')]
+    [CmdletBinding(PositionalBinding=$false)]
+    [switch]
     $Interactive
 )
 
@@ -38,4 +85,16 @@ $moduleName= 'VSDevCmd.psm1'
 $modulePath = Join-Path (Get-PSScriptLocationFullPath) $moduleName
 Import-Module $modulePath
 
-vsdevcmd $Command $Arguments -Interactive:$Interactive
+switch ($PSCmdlet.ParameterSetName) {
+    'CodeName' {
+        # Call using VisualStudioCodeName; Do not pass $VisualStudioVersion
+        Invoke-VsBuildCommand $Command -Arguments $Arguments -VisualStudioEdition $VisualStudioEdition -VisualStudioCodeName $VisualStudioCodeName -VisualStudioBuildVersion $VisualStudioBuildVersion -Interactive:$Interactive
+        Break;
+    }
+
+    Default {
+        # Call using $VisualStudioVersion; Do not pass $VisualStudioCodeName
+        Invoke-VsBuildCommand $Command -Arguments $Arguments -VisualStudioEdition $VisualStudioEdition -VisualStudioVersion $VisualStudioVersion -VisualStudioBuildVersion $VisualStudioBuildVersion -Interactive:$Interactive
+        Break;
+    }
+}
--- VSDevCmd/msbuild.ps1
+++ VSDevCmd/msbuild.ps1
@@ -7,6 +7,17 @@
     To see help for msbuild, call with '/?' (not '-?'). Also see examples.
 .PARAMETER Arguments
     List of arguments to pass to msbuild
+.PARAMETER VisualStudioEdition
+    Selects Visual Studio Development Environment based on Edition (Community, Professional, Enterprise, etc.)
+.PARAMETER VisualStudioVersion
+    Selects Visual Studio Development Environment based on Version (2015, 2017, 2019 etc.)
+.PARAMETER VisualStudioCodename
+    Selects Visual Studio Development Environment based on Version CodeName (Dev14, Dev15, Dev16 etc.)
+.PARAMETER VisualStudioBuildVersion
+    Selects Visual Studio Development Environment based on Build Version (e.g., "15.9.25", "16.8.0").
+    A prefix is sufficient (e.g., "15", "15.9", "16" etc.)
+.PARAMETER Interactive
+    Runs in interactive mode. Useful for running programs like cmd.exe, pwsh.exe, powershell.exe or csi.exe in the Visual Studio Developer Command Prompt Environment
 .EXAMPLE
     .\msbuild.ps1 Foo.proj /bl
     Builds project 'Foo.proj' and produces and binary-log 
@@ -14,10 +25,47 @@
     .\msbuild.ps1 /?
     Shows commandline help for msbuild.exe
 #>
+[CmdletBinding(DefaultParameterSetName='Default')]
 param (
-    [Parameter(Position=0, ValueFromRemainingArguments)]
+    [Parameter(ParameterSetName = 'Default', Position=0, ValueFromRemainingArguments, HelpMessage='List of arguments')]
+    [Parameter(ParameterSetName = 'CodeName', Position=0, ValueFromRemainingArguments, HelpMessage='List of arguments')]
     [string[]]
-    $Arguments
+    $Arguments,
+
+    [Parameter(ParameterSetName='Default', Mandatory = $false, HelpMessage='Selects Visual Studio Development Environment based on Edition (Community, Professional, Enterprise, etc.)')]
+    [Parameter(ParameterSetName='CodeName', Mandatory = $false, HelpMessage='Selects Visual Studio Development Environment based on Edition (Community, Professional, Enterprise, etc.)')]
+    [CmdletBinding(PositionalBinding=$false)]
+    [Alias('Edition')]
+    [ValidateSet('Community', 'Professional', 'Enteprise', $null)]
+    [string]
+    $VisualStudioEdition = $null,
+
+    [Parameter(ParameterSetName='Default', Mandatory = $false, HelpMessage='Selects Visual Studio Development Environment based on Version (2015, 2017, 2019 etc.)')]
+    [CmdletBinding(PositionalBinding=$false)]
+    [Alias('Version')]
+    [ValidateSet('2015', '2017', '2019', $null)]
+    [string]
+    $VisualStudioVersion = $null, 
+
+    [Parameter(ParameterSetName='CodeName', Mandatory = $false, HelpMessage='Selects Visual Studio Development Environment based on Version CodeName (Dev14, Dev15, Dev16 etc.)')]
+    [CmdletBinding(PositionalBinding=$false)]
+    [Alias('CodeName')]
+    [ValidateSet('Dev14', 'Dev15', 'Dev16', $null)]
+    [string]
+    $VisualStudioCodeName=$null, 
+
+    [Parameter(ParameterSetName='Default', Mandatory = $false, HelpMessage='Selects Visual Studio Development Environment based on Build Version (e.g., "15.9.25", "16.8.0"). A prefix is sufficient (e.g., "15", "15.9", "16" etc.)')]
+    [Parameter(ParameterSetName='CodeName', Mandatory = $false, HelpMessage='Selects Visual Studio Development Environment based on Build Version (e.g., "15.9.25", "16.8.0"). A prefix is sufficient (e.g., "15", "15.9", "16" etc.)')]
+    [Alias('BuildVersion')]
+    [CmdletBinding(PositionalBinding=$false)]
+    [string]
+    $VisualStudioBuildVersion = $null, 
+
+    [Parameter(ParameterSetName='Default', HelpMessage='Runs in interactive mode. Useful for running programs like cmd.exe, pwsh.exe, powershell.exe or csi.exe in the Visual Studio Developer Command Prompt Environment')]
+    [Parameter(ParameterSetName='CodeName', HelpMessage='Runs in interactive mode. Useful for running programs like cmd.exe, pwsh.exe, powershell.exe or csi.exe in the Visual Studio Developer Command Prompt Environment')]
+    [CmdletBinding(PositionalBinding=$false)]
+    [switch]
+    $Interactive
 )
 
 Function Get-PSScriptLocationFullPath {
@@ -32,9 +80,16 @@ $moduleName= 'VSDevCmd.psm1'
 $modulePath = Join-Path (Get-PSScriptLocationFullPath) $moduleName
 Import-Module $modulePath -Scope Local
 
-try {
-    msbuild $Arguments
-} catch {
-    $_.ScriptStackTrace
-    throw
+switch ($PSCmdlet.ParameterSetName) {
+    'CodeName' {
+        # Call using VisualStudioCodeName; Do not pass $VisualStudioVersion
+        Invoke-MsBuild -Arguments $Arguments -VisualStudioEdition $VisualStudioEdition -VisualStudioCodeName $VisualStudioCodeName -VisualStudioBuildVersion $VisualStudioBuildVersion -Interactive:$Interactive
+        Break;
+    }
+
+    Default {
+        # Call using $VisualStudioVersion; Do not pass $VisualStudioCodeName
+        Invoke-MsBuild -Arguments $Arguments -VisualStudioEdition $VisualStudioEdition -VisualStudioVersion $VisualStudioVersion -VisualStudioBuildVersion $VisualStudioBuildVersion -Interactive:$Interactive
+        Break;
+    }
 }
